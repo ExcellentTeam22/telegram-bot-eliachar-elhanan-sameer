443d7da309a5569573b5151bf4976438ec77d431
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-08-18 12:16:28 +0300

    Update main.py
    factorial function

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from flask import Flask, Response, request
 app = Flask(__name__)
 
 TOKEN = '5661110898:AAH9WEsgFUSx7bLKQBtIcs4lHuw6aEbTSw0'
-TELEGRAM_INIT_WEBHOOK_URL = "https://api.telegram.org/bot{}/setWebhook?url=https://3321-37-142-167-82.eu.ngrok.io/message".format(TOKEN)
+TELEGRAM_INIT_WEBHOOK_URL = "https://api.telegram.org/bot{}/setWebhook?url=https://f3f9-2-53-16-160.eu.ngrok.io/message".format(
+    TOKEN)
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+
 
 # @app.route('/message', methods=["POST"])
 # def handle_message():
@@ -26,6 +28,26 @@ def handle_message():
     msg_from_usr = request.get_json()['message']['text'].split(' ')
     if msg_from_usr[0] == '/prime':
         handle_prime(msg_from_usr[1])
+    if msg_from_usr[0] == '/factorial':
+        handle_factorial(msg_from_usr[1])
+    return Response("success")
+
+
+@app.route('/factorial', methods=["POST"])
+def handle_factorial(num: int):
+    chat_id = request.get_json()['message']['chat']['id']
+    num = int(num)
+    number = 1
+    counter = 2
+    while number < num:
+        number *= counter
+        counter += 1
+    if number == num & num != 1:
+        res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+                           .format(TOKEN, chat_id, f"factorial of {counter - 1}"))
+    else:
+        res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+                           .format(TOKEN, chat_id, "not factorial"))
 
     return Response("success")
 
@@ -57,4 +79,3 @@ def is_prime(num: int):
 
 if __name__ == '__main__':
     app.run(port=5002)
-
