3fa242cc3c38da97dda9e72eabad984663f7d258
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-08-21 10:04:17 +0300

    connect to moovit

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-TOKEN = '5661110898:AAH9WEsgFUSx7bLKQBtIcs4lHuw6aEbTSw0'
-ADDRESS = 'https://f080-2a10-8012-f-7660-a445-1531-de61-7f9a.eu.ngrok.io'
+TOKEN = '5465265510:AAHJBqcEvaJ0L7x2y-4P_fOkr_qxxRXrJ2U'
+ADDRESS = 'https://1e8e-2a0d-6fc7-330-2de1-99d4-be06-2fac-69be.eu.ngrok.io'
 TELEGRAM_INIT_WEBHOOK_URL = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={ADDRESS}/message"
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
 
 @app.route('/message', methods=["POST"])
 def handle_message():
+    bus_line=0
+    city=""
     print("got msg")
     chat_id = request.get_json()['message']['chat']['id']
     if request.get_json()['message']['text']:
-        msg_from_usr = request.get_json()['message']['text'].split(' ')
+        bus_line,city = request.get_json()['message']['text'].split(' ')
 
+        print([bus_line,city])
     return Response("success")
 
 
