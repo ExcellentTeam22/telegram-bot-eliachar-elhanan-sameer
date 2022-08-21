5b58022ac56c4d28e501778e07174e9236520820
sameerjbara <92097672+sameerjbara@users.noreply.github.com>
2022-08-21 13:56:28 +0300

    div the routes by stations

--- connect_to_moovit.py
+++ connect_to_moovit.py
@@ -1,51 +1,41 @@
 import bs4
 import requests
 import re
+
 from text_unidecode import unidecode
 
-
-
 site = requests.get(
-    "https://moovitapp.com/index/en/public_transit-lines-Israel-1-1")
+        "https://moovitapp.com/index/en/public_transit-lines-Israel-1-1")
 soup = bs4.BeautifulSoup(site.text, 'html.parser')
 
-# Downloading main page and finding links
+    # Downloading main page and finding links
 links2 = soup.select('div[class="lines-container agency-lines"] a[href]')
-while (True):
-    select = input("Enter bus number (q to quit): ")
-    if (select.upper() == "Q"):
-        break
+links=[]
+def connect_mooivt(line):
+
+    bus_lines = []
+
+    select = line
+
     web = ""
     bus = re.compile(r'\d+')
-    bus_lines = []
     for i in range(0, len(links2)):
         # Finding link with bus number
         if (bus.search(links2[i].get('href')).group() == select):
             bus_lines.append(links2[i])
 
-    for index, i in enumerate(bus_lines):
-        print(index,"--",i.getText().replace('\n',"")[2:])
-    bus_line_index = 0
-    if len(bus_lines) > 1:
-        bus_line_index = input("witch one ? ")
+    return bus_lines
 
-    web = links2[int(bus_line_index)].get('href')
-
+def specific_line(index):
+    web = links2[int(index)].get('href')
     site = requests.get("https://moovitapp.com/index/en/" + web)
-    # Downloading page with bus stops
     soup = bs4.BeautifulSoup(site.text, 'html.parser')
     links = soup.select('ul[class="stops-list bordered"] h3')
-    stops = []
-    e = 1
 
-    for index in range(0, len(links)):
-        print(index,"--",links[index].getText())
+    return links
 
-    start = input("Enter start station: ")
-    end = input("Enter end station: ")
 
-    start_end = links[int(start):int(end)]
-    for station in start_end:
-        print(station.getText())
-
+def plan_drive(start,end):
+    for i in links[start:end]:
+        print(i)
 
--- main.py
+++ main.py
@@ -1,24 +1,60 @@
 import requests
 from flask import Flask, Response, request
+import connect_to_moovit as cm
+import WazeRouteCalculator as w
+
 
 app = Flask(__name__)
 
-TOKEN = '5465265510:AAHJBqcEvaJ0L7x2y-4P_fOkr_qxxRXrJ2U'
-ADDRESS = 'https://1e8e-2a0d-6fc7-330-2de1-99d4-be06-2fac-69be.eu.ngrok.io'
+TOKEN = '5626802621:AAH7H4G6rSskt18t6eElROOcwX_nFc7XxW0'
+ADDRESS = 'https://247b-2a0d-6fc7-330-2de1-99d4-be06-2fac-69be.eu.ngrok.io'
 TELEGRAM_INIT_WEBHOOK_URL = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={ADDRESS}/message"
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
+stations = []
+my_list = []
+city = ""
 
 
 @app.route('/message', methods=["POST"])
 def handle_message():
-    bus_line=0
-    city=""
+    global stations
+    global city
+    bus_line = 0
+
     print("got msg")
     chat_id = request.get_json()['message']['chat']['id']
     if request.get_json()['message']['text']:
-        bus_line,city = request.get_json()['message']['text'].split(' ')
+        msg = request.get_json()['message']['text'].split(' ')
+        command = msg[0]
 
-        print([bus_line,city])
+        if command == "/choseLine":
+            all_buses = cm.connect_mooivt((msg[1]))
+            city = msg[2]
+            for index, i in enumerate(all_buses):
+                str = i.getText().replace('\n', "")[2:]
+                res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+                                   .format(TOKEN, chat_id, f"{index}--{str}"))
+
+        if command == "/specificLine":
+            line_index = msg[1]
+            stations = cm.specific_line(line_index)
+            for index, i in enumerate(stations):
+                str = i.getText().replace('\n', "")
+                res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+                                   .format(TOKEN, chat_id, f"{index}--{str}"))
+
+        if command == "/plan":
+
+            start = int(msg[1])
+            end = int(msg[2])
+            for index, i in enumerate(stations[start:end]):
+                str = i.getText().replace('\n', "")
+                res = requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
+                                   .format(TOKEN, chat_id, f"{index}--{str}"))
+                my_list.append([str,city])
+        if command=="\estimatedTime":
+            print(1)
+
     return Response("success")
 
 
